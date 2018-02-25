09fbdeb21ec24fb37906eb49aebf4df81c661759
Germano Barcelos <germanobarcelos@yahoo.com.br>
2018-02-25 20:32:30 -0300

    Finishing All

--- HelloWorld/app/src/main/java/gegen07/github/io/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/gegen07/github/io/helloworld/MainActivity.java
@@ -3,34 +3,38 @@ package gegen07.github.io.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final EditText message = findViewById(R.id.message);
-        final Button display_button = findViewById(R.id.display_button);
+        final Button displayButton = findViewById(R.id.display_button);
 
-        display_button.setOnClickListener(new View.OnClickListener() {
+        displayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String message_caught = message.getText().toString();
-                if (!TextUtils.isEmpty(message_caught) || !message_caught.equals("")) {
+                String messageCaught = message.getText().toString();
+                if (!TextUtils.isEmpty(messageCaught) || !messageCaught.equals("")) {
                     Toast.makeText(MainActivity.this,
-                            message_caught,
+                            messageCaught,
                             Toast.LENGTH_SHORT)
                             .show();
-                } else Toast.makeText(MainActivity.this,
-                        "Escreva uma mensagem",
-                        Toast.LENGTH_SHORT)
-                        .show();
+                } else {
+                    Toast.makeText(MainActivity.this,
+                            "Escreva uma mensagem",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                    Log.i(LOG_TAG, "Caixa de texto vazia!");
+                }
             }
         });
         
