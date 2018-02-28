997ca7e6e220d11c053ba87974fad7e899c0693d
icaro emidio <a2016951600@la0012217621.infocoltec.local>
2018-02-28 14:04:15 -0300

    Exercico 1 Hello Word

---
+++ HelloActivity.java
@@ -0,0 +1,36 @@
+package br.ufmg.coltec.hellodroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+
+        //Instancia do botão//
+        final EditText text = findViewById(R.id.text);
+        Button btnLogin = findViewById(R.id.btn_login);
+
+        if (!text.equals("")) {
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(HelloActivity.this, text.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else{
+            Log.i(String.valueOf(text), "onCreate: Error");
+        }
+    }
+}
