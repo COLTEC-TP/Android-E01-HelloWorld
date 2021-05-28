4b9f380ffb123bc83a9c286a393cb638d184e609
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2021-05-28 00:09:16 -0300

    função de inserir nome + toast implementada

--- app/src/main/java/br/ufmg/coltec/hellodroid/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/hellodroid/MainActivity.java
@@ -2,13 +2,40 @@ package br.ufmg.coltec.hellodroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Context context;
+    private InputMethodManager inputMethodManager;
+    private TextView txt;
+    private EditText input;
+    private Button btnSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+        inputMethodManager = (InputMethodManager) getSystemService(context.INPUT_METHOD_SERVICE);
+
+        txt =  findViewById(R.id.txt_hello_world);
+        input = findViewById(R.id.txt_input);
+        btnSend = findViewById(R.id.btn_send);
+
+        btnSendOnClick();
+    }
+
+    private void btnSendOnClick(){
+        btnSend.setOnClickListener(view -> {
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            Toast toast = Toast.makeText(context, input.getText(), Toast.LENGTH_SHORT);
+            toast.show();
+        });
     }
 }
